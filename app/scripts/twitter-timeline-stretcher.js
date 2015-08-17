356b56a0024ae14b65c1d648ee093ce8b983c89e
Pat Sissons <patricksissons@gmail.com>
2015-08-17 13:16:50 -0600

    we don't actually need to adjust the margin here

--- app/scripts/twitter-timeline-stretcher.js
+++ app/scripts/twitter-timeline-stretcher.js
@@ -4,7 +4,6 @@ window.onload = function() {
   var div = document.getElementsByClassName('twitter-timeline')[0].contentDocument.getElementsByClassName('twitter-timeline')[0];
   var stream = div.getElementsByClassName('stream')[0];
   div.style.maxWidth = '100%';
-  div.style.marginBottom = '0';
   // 17px puts the scrollbar off screen
   stream.style.width = (div.offsetWidth + 17) + 'px';
 };
